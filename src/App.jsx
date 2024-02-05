2d84dd7c64b914fc1ebd7f6fd4698039cfc366d0
Waseem Mohammad <wmohammad83@icloud.com>
2024-02-05 22:16:32 +0000

    global styles setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.5rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-sm);
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 0.8rem 1.5rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
 
 function App() {
   return (
-    <div>
-      Hello world 
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("check In")}>Check In</Button>
+        <Button onClick={() => alert("check Out")}>Check Out</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
-export default App
+export default App;
