32bf6859014582e81e4bd47d715812d7d7215e55
Waseem Mohammad <wmohammad83@icloud.com>
2024-02-06 20:55:19 +0000

    styled components for buttons and input

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import  Button  from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.5rem;
-  font-weight: 500;
-  border: none;
-  border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
-  box-shadow: var(--shadow-sm);
-  cursor: pointer;
-  margin: 20px;
-`;
 
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  padding: 0.8rem 1.5rem;
-`;
+ 
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -37,9 +19,11 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Check in and out</Heading>
         <Button onClick={() => alert("check In")}>Check In</Button>
         <Button onClick={() => alert("check Out")}>Check Out</Button>
+        <Heading as="h3">Form</Heading>
         <Input type="number" placeholder="Number of guests" />
       </StyledApp>
     </>
