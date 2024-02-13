28e2d2b6300cb5b23ec6c5d2ce7924d831294ba3
Waseem Mohammad <wmohammad83@icloud.com>
2024-02-13 22:42:27 +0000

    delete cabin function

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,14 +1,24 @@
 import supabase from "./supabase";
 
 export async function getCabins() {
-const { data, error } = await supabase
-.from('cabins')
-.select('*')
+  const { data, error } = await supabase.from("cabins").select("*");
 
-if(error) {
-    throw new Error("Cabins could not be found")
-    console.error(error)
+  if (error) {
+    throw new Error("Cabins could not be found");
+    console.error(error);
+  }
+  return data;
 }
 
-return data;
+export async function deleteCabin(id) {
+  const { data ,error } = await supabase
+    .from("cabins")
+    .delete()
+    .eq("id", id);
+
+    if (error) {
+        throw new Error("Cabins could not be deleted");
+        console.error(error);
+      }
+      return data;
 }
