ab317cdb5523db354cbd9fafa4406f631daac36b
Waseem Mohammad <wmohammad83@icloud.com>
2024-02-13 22:05:11 +0000

    fetching cabin data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,20 +1,23 @@
-import { useEffect } from "react";
+
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
 
-  useEffect(function() {
-    getCabins().then(data => console.log(data))
-  },[]);
+
 
   return (
+    <>
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://hoejqdlibyhxlkekbtqm.supabase.co/storage/v1/object/public/cabin-images/cabin-002.jpg" alt="cabin" />
+      <p>Filter/Sort</p>
     </Row>
+
+    <Row>
+      <CabinTable /> 
+    </Row>
+    </>
   );
 }
 
